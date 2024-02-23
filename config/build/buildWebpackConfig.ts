71ee6a37dca694a20d2ff3c9964cd8bbb7e48b58
AlimBodzhgua <bodzhgua2015@mail.ru>
2024-02-23 12:21:47 +0300

    initial commit

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,28 @@
+import webpack from 'webpack';
+import path from 'path';
+import { BuildOptions } from "./types/config"
+import { buildLoaders } from './buildLoaders';
+import { buildPlugins } from './buildPlugins';
+import { buildResolvers } from './buildResolvers';
+import { buildDevServer } from './buildDevServer';
+
+export const buildWebpackConfig = (options: BuildOptions): webpack.Configuration => {
+	const { mode, paths, isDev } = options;
+
+	return {
+		mode,
+		entry: paths.entry,
+		output: {
+			filename: '[name].[contenthash].js',
+			path: paths.build,
+			clean: true,
+		},
+		module: {
+			rules: buildLoaders(options),
+		},
+		plugins: buildPlugins(options),
+		resolve: buildResolvers(),
+		devtool: isDev ? 'inline-source-map' : undefined,
+		devServer: isDev ? buildDevServer(options) : undefined,
+	}
+}
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './index.scss';
+
+export const App = () => {
+	return (
+		<div className='app'>
+			<h1>Welcome</h1>
+		</div>
+	)
+}

3c11af6013ca4b7717c2ebf4e1d88eee3c21c6d2
AlimBodzhgua <bodzhgua2015@mail.ru>
2024-03-10 21:45:42 +0300

    add error boundary and page error

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import ErrorBoundary from 'components/ErrorBoundary/ErrorBoundary';
 import { App } from './App';
 import { createReduxStore } from './redux/config/store';
 import 'config/i18n/i18n';
@@ -10,9 +11,12 @@ const root = createRoot(document.getElementById('root')!);
 const store = createReduxStore();
 
 root.render(
-    <BrowserRouter>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </BrowserRouter>,
+    <ErrorBoundary>
+        <BrowserRouter>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </BrowserRouter>
+        ,
+    </ErrorBoundary>,
 );

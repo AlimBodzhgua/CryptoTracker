b39fc1421a3f925fc4d1d8b7c047171f09d79309
AlimBodzhgua <bodzhgua2015@mail.ru>
2024-04-14 17:54:40 +0300

    deleted unnecessary data save in localstorage

--- src/components/History/HistoryList/HistoryList.tsx
+++ src/components/History/HistoryList/HistoryList.tsx
@@ -2,16 +2,17 @@ import { FC, useCallback, memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Button, ButtonSize } from 'components/UI/Button/Button';
 import {
-    selectUser, selectUserConversionHistory, selectUserError, selectUserIsLoading,
+    selectUserConversionHistory,
+    selectUserError,
+    selectUserIsLoading,
 } from 'redux/selectors/userSelectors';
 import { useAppDispatch, useAppSelector } from 'hooks/redux';
 import { clearHistory } from 'redux/actions/userActions';
-import { USER_LOCALSTORAGE_KEY } from 'constants/localStorage';
 import { Message } from 'components/UI/Message/Message';
 import { Skeleton } from 'components/UI/Skeleton/Skeleton';
-import classnames from 'classnames';
 import { HistoryItem } from '../HistoryItem/HistoryItem';
 
+import classnames from 'classnames';
 import classes from './HistoryList.module.scss';
 
 interface HistoryListProps {
@@ -20,28 +21,14 @@ interface HistoryListProps {
 
 const HistoryList: FC<HistoryListProps> = memo(({ className }) => {
     const { t } = useTranslation();
-    const user = useAppSelector(selectUser);
-    const isLoading = useAppSelector(selectUserIsLoading);
-    const error = useAppSelector(selectUserError);
     const dispatch = useAppDispatch();
     const history = useAppSelector(selectUserConversionHistory);
+    const isLoading = useAppSelector(selectUserIsLoading);
+    const error = useAppSelector(selectUserError);
 
     const onClear = useCallback(async () => {
-        const { meta } = await dispatch(clearHistory());
-        if (meta.requestStatus === 'fulfilled') {
-            localStorage.setItem(USER_LOCALSTORAGE_KEY, JSON.stringify({ ...user, conversionHistory: [] }));
-        }
+        dispatch(clearHistory());
     }, [dispatch]);
-
-    if (error) {
-        return (
-            <Message
-                withIcon
-                type='error'
-                text='Error loading history, try to reload the page'
-            />
-        );
-    }
 
     const renderListSkeleton = useCallback(() => (
         new Array(3).fill(0).map((_, index) => (
@@ -66,6 +53,16 @@ const HistoryList: FC<HistoryListProps> = memo(({ className }) => {
             </h3>
         );
     }, [history]);
+
+    if (error) {
+        return (
+            <Message
+                withIcon
+                type='error'
+                text='Error loading history, try to reload the page'
+            />
+        );
+    }
 
     return (
         <>

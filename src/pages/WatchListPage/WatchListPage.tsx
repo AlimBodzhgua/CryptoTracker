fa6322cb4e5a8b4273a9959fb549648b1fa9565b
AlimBodzhgua <bodzhgua2015@mail.ru>
2024-04-16 19:15:52 +0300

    fixed watchlist incorrect fetch error

--- src/pages/WatchListPage/WatchListPage.tsx
+++ src/pages/WatchListPage/WatchListPage.tsx
@@ -1,8 +1,9 @@
 import { FC, useEffect } from 'react';
-import { useAppDispatch } from 'hooks/redux';
+import { useAppDispatch, useAppSelector } from 'hooks/redux';
 import { fetchWatchListCoins } from 'redux/actions/userActions';
 import { Page } from 'components/UI/Page/Page';
 import { WatchList } from 'components/WatchList/WatchList';
+import { selectUserMounted } from 'redux/selectors/userSelectors';
 
 import classnames from 'classnames';
 import classes from './WatchListPage.module.scss';
@@ -13,12 +14,15 @@ interface WatchListPageProps {
 
 const WatchListPage: FC<WatchListPageProps> = ({ className }) => {
     const dispatch = useAppDispatch();
+    const userMounted = useAppSelector(selectUserMounted);
 
     useEffect(() => {
         if (__PROJECT__ !== 'storybook') {
-            dispatch(fetchWatchListCoins());
+            if (userMounted) {
+                dispatch(fetchWatchListCoins());
+            }
         }
-    }, [dispatch]);
+    }, [dispatch, userMounted]);
 
     return (
         <Page className={classnames(classes.WatchListPage, className)}>

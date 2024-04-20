1fe51d779c10cf24abd276ffcd85e88e6f51df59
AlimBodzhgua <bodzhgua2015@mail.ru>
2024-04-20 10:39:55 +0300

    formatter locale now depends on i18 language

--- src/hooks/useFormatter.ts
+++ src/hooks/useFormatter.ts
@@ -1,25 +1,25 @@
-import { selectCurrentCurrency } from 'redux/selectors/currencySelectors';
 import { useMemo } from 'react';
+import { selectCurrentCurrency } from 'redux/selectors/currencySelectors';
 import { useAppSelector } from './redux';
+import { useTranslation } from 'react-i18next';
 
 type NotationType = 'standard' | 'scientific' | 'engineering' | 'compact' | undefined;
-type LocaleType = 'ru' | 'en' | undefined;
 
 export const useFormatter = (
-    locale:LocaleType = 'ru',
     notation:NotationType = 'compact',
     minDigits:number = 4,
     maxDigits:number = 6,
 ) => {
     const currentCurrency = useAppSelector(selectCurrentCurrency);
+    const { i18n } = useTranslation();
 
-    const formatter = useMemo(() => Intl.NumberFormat(locale, {
+    const formatter = useMemo(() => Intl.NumberFormat(i18n.language, {
         style: 'currency',
         currency: currentCurrency,
         notation,
         minimumSignificantDigits: minDigits || undefined,
         maximumSignificantDigits: maxDigits || undefined,
-    }), [currentCurrency, locale, notation, minDigits, maxDigits]);
+    }), [currentCurrency, i18n.language, notation, minDigits, maxDigits]);
 
     return formatter;
 };

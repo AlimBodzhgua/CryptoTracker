f6a5dd01f7acd5fa1b9aa4029e5efef300827ddf
AlimBodzhgua <bodzhgua2015@mail.ru>
2024-04-23 20:41:21 +0300

    deleted unused import

--- src/components/Sidebar/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar/Sidebar.tsx
@@ -1,14 +1,15 @@
-import React, { memo } from 'react';
+import { FC, memo } from 'react';
 import { sidebarList } from 'constants/sidebarList';
-import classnames from 'classnames';
 import { SidebarItem } from '../SidebarItem/SidebarItem';
+
+import classnames from 'classnames';
 import classes from './Sidebar.module.scss';
 
 interface SidebarProps {
 	className?: string;
 }
 
-export const Sidebar: React.FC<SidebarProps> = memo(({ className }) => (
+export const Sidebar: FC<SidebarProps> = memo(({ className }) => (
     <aside className={classnames(classes.Sidebar, className)}>
         <h1 className={classes.header}>Coin Cap</h1>
         <div className={classes.menu}>

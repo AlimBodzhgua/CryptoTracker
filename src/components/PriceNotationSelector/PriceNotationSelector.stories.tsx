58cf3553fd433efeff55222148722354cf132d10
AlimBodzhgua <bodzhgua2015@mail.ru>
2024-04-23 19:31:20 +0300

    added stories

---
+++ src/components/PriceNotationSelector/PriceNotationSelector.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { PriceNotationSelector } from './PriceNotationSelector';
+
+const meta = {
+    title: 'Components/PriceNotationSelector',
+    component: PriceNotationSelector,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+    argTypes: {},
+} satisfies Meta<typeof PriceNotationSelector>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+    args: {},
+};
---
+++ src/components/TagsSelector/TagsSelector.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { TagsSelector } from './TagsSelector';
+
+const meta = {
+    title: 'Components/TagsSelector',
+    component: TagsSelector,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+    argTypes: {},
+} satisfies Meta<typeof TagsSelector>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+    args: {},
+};

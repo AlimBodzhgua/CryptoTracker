3765e83a9eaba7b4da07de41cc53eae9712ae37a
AlimBodzhgua <bodzhgua2015@mail.ru>
2024-05-09 18:09:19 +0300

    storybook update

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -24,6 +24,7 @@ export default ({ config }: {config: webpack.Configuration}) => {
 
         config.resolve.extensions!.push('.ts', '.tsx');
         config.resolve.modules!.push(paths.src);
+        config.resolve.fallback = { "timers": require.resolve("timers-browserify")};
     }
 
     if (config.module) {

04a574cc4fb17b9928b12394cbff69fdd502fd8d
AlimBodzhgua <bodzhgua2015@mail.ru>
2024-05-15 10:47:41 +0300

    lint fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { useAppDispatch, useAppSelector } from 'hooks/redux';
 import { FC, useEffect } from 'react';
 import { fetchCurrency } from 'store/actions/currencyActions';
 import {
-    selectCurrencyKurs,
-    selectCurrentCurrency,
-    selectTargetCurrency,
+	selectCurrencyKurs,
+	selectCurrentCurrency,
+	selectTargetCurrency,
 } from 'store/selectors/currencySelectors';
 import { coinsActions } from 'store/slices/coinsSlice';
 import { currencyActions } from 'store/slices/currencySlice';
@@ -15,32 +15,32 @@ import { initUserAuth } from 'store/actions/userActions';
 import 'styles/index.scss';
 
 export const App: FC = () => {
-    const dispatch = useAppDispatch();
-    const targetCurrency = useAppSelector(selectTargetCurrency);
-    const currentCurrency = useAppSelector(selectCurrentCurrency);
-    const kurs = useAppSelector(selectCurrencyKurs);
+	const dispatch = useAppDispatch();
+	const targetCurrency = useAppSelector(selectTargetCurrency);
+	const currentCurrency = useAppSelector(selectCurrentCurrency);
+	const kurs = useAppSelector(selectCurrencyKurs);
 
-    useEffect(() => {
-        dispatch(fetchCurrency());
-        const user = localStorage.getItem(USER_LOCALSTORAGE_KEY);
-        if (user) {
-            const authData = JSON.parse(user) as IUser;
-            dispatch(initUserAuth(authData.id));
-        }
-    }, [dispatch]);
+	useEffect(() => {
+		dispatch(fetchCurrency());
+		const user = localStorage.getItem(USER_LOCALSTORAGE_KEY);
+		if (user) {
+			const authData = JSON.parse(user) as IUser;
+			dispatch(initUserAuth(authData.id));
+		}
+	}, [dispatch]);
 
-    useEffect(() => {
-        if (kurs && targetCurrency) {
-            dispatch(coinsActions.changeCoinsCurrency({
-                kurs,
-                currentCurrency,
-                targetCurrency,
-            }));
-            dispatch(currencyActions.resetCurrentCurrency());
-        }
-    }, [dispatch, targetCurrency]);
+	useEffect(() => {
+		if (kurs && targetCurrency) {
+			dispatch(coinsActions.changeCoinsCurrency({
+				kurs,
+				currentCurrency,
+				targetCurrency,
+			}));
+			dispatch(currencyActions.resetCurrentCurrency());
+		}
+	}, [dispatch, targetCurrency]);
 
-    return (
-        <AppRouter />
-    );
+	return (
+		<AppRouter />
+	);
 };

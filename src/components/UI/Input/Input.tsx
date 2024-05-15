9c80e66183ef217f2679d7f20b5e2e57e14c83ef
AlimBodzhgua <bodzhgua2015@mail.ru>
2024-05-15 16:47:19 +0300

    deleted memo

--- src/components/UI/Input/Input.tsx
+++ src/components/UI/Input/Input.tsx
@@ -4,7 +4,6 @@ import {
 	ReactElement,
 	ReactNode,
 	forwardRef,
-	memo,
 } from 'react';
 import { useFocus } from 'hooks/useFocus';
 import classnames from 'classnames';
@@ -17,37 +16,35 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 	fieldClassName?: string;
 }
 
-export const Input = memo(
-	forwardRef<HTMLInputElement, InputProps>((props, ref) => {
-		const {
-			className,
-			addonBefore,
-			addonAfter,
-			fieldClassName,
-			...otherProps
-		} = props;
-		const [isFocused, focusProps] = useFocus();
+export const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+	const {
+		className,
+		addonBefore,
+		addonAfter,
+		fieldClassName,
+		...otherProps
+	} = props;
+	const [isFocused, focusProps] = useFocus();
 
-		const style: CSSProperties = {
-			backgroundColor: isFocused ? '#717171' : '#454545',
-		};
+	const style: CSSProperties = {
+		backgroundColor: isFocused ? '#717171' : '#454545',
+	};
 
-		return (
-			<div
-				className={classnames(classes.Input, className)}
-				style={style}
-				data-testid='inputWrapper'
-			>
-				{addonBefore}
-				<input
-					className={classnames(classes.InputField, fieldClassName)}
-					ref={ref}
-					data-testid='inputField'
-					{...otherProps}
-					{...focusProps}
-				/>
-				{addonAfter}
-			</div>
-		);
-	}),
-);
+	return (
+		<div
+			className={classnames(classes.Input, className)}
+			style={style}
+			data-testid='inputWrapper'
+		>
+			{addonBefore}
+			<input
+				className={classnames(classes.InputField, fieldClassName)}
+				ref={ref}
+				data-testid='inputField'
+				{...otherProps}
+				{...focusProps}
+			/>
+			{addonAfter}
+		</div>
+	);
+});
--- src/components/UI/LoaderRing/LoaderRing.tsx
+++ src/components/UI/LoaderRing/LoaderRing.tsx
@@ -5,19 +5,19 @@ import classnames from 'classnames';
 import classes from './LoaderRing.module.scss';
 
 interface LoaderRingProps {
-    className?: string;
+	className?: string;
 }
 
 export const LoaderRing: FC<LoaderRingProps> = ({ className }) => (
-    <div className={classnames(classes.LoaderRing, className)}>
-        <ColorRing
-            visible
-            height='80'
-            width='80'
-            ariaLabel='color-ring-loading'
-            wrapperStyle={{}}
-            wrapperClass={classnames(classes.LoaderRing, className)}
-            colors={['#e6e6e6', '#cccccc', '#b3b3b3', '#bfbfbf', '#e6e6e6']}
-        />
-    </div>
+	<div className={classnames(classes.LoaderRing, className)}>
+		<ColorRing
+			visible
+			height='80'
+			width='80'
+			ariaLabel='color-ring-loading'
+			wrapperStyle={{}}
+			wrapperClass={classnames(classes.LoaderRing, className)}
+			colors={['#e6e6e6', '#cccccc', '#b3b3b3', '#bfbfbf', '#e6e6e6']}
+		/>
+	</div>
 );

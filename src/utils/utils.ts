1deb87ef98da2676da29cae475a3d3ecd539420d
AlimBodzhgua <bodzhgua2015@mail.ru>
2024-05-15 10:51:57 +0300

    lint fix

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -4,16 +4,16 @@ import { ConverterCoinType } from 'types/converter';
 import { IUser } from 'types/user';
 
 export const coinsSorter = (
-    coins: ICoin[],
-    sortDirection: SortDirectionType,
-    fieldName: FieldNameType,
+	coins: ICoin[],
+	sortDirection: SortDirectionType,
+	fieldName: FieldNameType,
 ) => [...coins].sort((a, b) => {
-    if (fieldName === 'name') {
-        if (sortDirection === 'descending') {
-            return a[`${fieldName}`].localeCompare(b[`${fieldName}`]);
-        }
-        return b[`${fieldName}`].localeCompare(a[`${fieldName}`]);
-    }
+	if (fieldName === 'name') {
+		if (sortDirection === 'descending') {
+			return a[`${fieldName}`].localeCompare(b[`${fieldName}`]);
+		}
+		return b[`${fieldName}`].localeCompare(a[`${fieldName}`]);
+	}
 	        if (sortDirection === 'descending') {
 	            return Number(a[`${fieldName}`]) > Number(b[`${fieldName}`]) ? -1 : 1;
 	        }
@@ -21,32 +21,32 @@ export const coinsSorter = (
 });
 
 export const createHistoryDoc = (
-    coinFrom: ConverterCoinType,
-    coinTo: ConverterCoinType,
-    amount: number,
-    result: number,
+	coinFrom: ConverterCoinType,
+	coinTo: ConverterCoinType,
+	amount: number,
+	result: number,
 ) => ({
-    coinFrom: {
-        symbol: coinFrom.symbol,
-        name: coinFrom.name,
-        iconUrl: coinFrom.iconUrl,
-    },
-    coinTo: {
-        symbol: coinTo.symbol,
-        name: coinTo.name,
-        iconUrl: coinTo.iconUrl,
-    },
-    amount,
-    convertResult: result,
+	coinFrom: {
+		symbol: coinFrom.symbol,
+		name: coinFrom.name,
+		iconUrl: coinFrom.iconUrl,
+	},
+	coinTo: {
+		symbol: coinTo.symbol,
+		name: coinTo.name,
+		iconUrl: coinTo.iconUrl,
+	},
+	amount,
+	convertResult: result,
 });
 
 export const getUserDataObject = (data: UserCredential):IUser => ({
-    id: data.user.uid,
-    email: data.user.email!,
-    login: data.user.displayName || '',
-    imageUrl: data.user.photoURL || '',
-    password: data.user.refreshToken,
-    isEmailVerified: data.user.emailVerified,
-    conversionHistory: [],
-    watchList: { ids: [], coins: [] },
+	id: data.user.uid,
+	email: data.user.email!,
+	login: data.user.displayName || '',
+	imageUrl: data.user.photoURL || '',
+	password: data.user.refreshToken,
+	isEmailVerified: data.user.emailVerified,
+	conversionHistory: [],
+	watchList: { ids: [], coins: [] },
 });

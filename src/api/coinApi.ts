57a5f35b6bff97686385c7e26e55b59beebdc35b
AlimBodzhgua <bodzhgua2015@mail.ru>
2024-05-15 10:43:48 +0300

    lint fix

--- src/api/coinApi.ts
+++ src/api/coinApi.ts
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 const coinApiHeaders = {
-    'Content-Type': 'application/json',
-    'x-access-token': process.env.API_KEY,
+	'Content-Type': 'application/json',
+	'x-access-token': process.env.API_KEY,
 };
 
 const coinApi = axios.create({
 	baseURL: 'https://api.coinranking.com/v2',
-	headers: coinApiHeaders, 
-}) 
+	headers: coinApiHeaders,
+});
 
 export default coinApi;

e8be2bf5573bb0897f1d2de7fe0fb2a94e25da43
AlimBodzhgua <bodzhgua2015@mail.ru>
2024-05-15 10:28:45 +0300

    lint fix

--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -1,13 +1,13 @@
 import {
-    FC, MutableRefObject, useEffect, useRef,
+	FC, MutableRefObject, useEffect, useRef,
 } from 'react';
 import { useTranslation } from 'react-i18next';
 import {
-    TimeChartOptions,
-    UTCTimestamp,
-    createChart,
-    ColorType,
-    isUTCTimestamp,
+	TimeChartOptions,
+	UTCTimestamp,
+	createChart,
+	ColorType,
+	isUTCTimestamp,
 } from 'lightweight-charts';
 
 import classnames from 'classnames';
@@ -19,48 +19,48 @@ interface ChartProps {
 }
 
 export const Chart: FC<ChartProps> = (props) => {
-    const { className, sparkline } = props;
-    const { t } = useTranslation();
-    const chartContainerRef = useRef() as MutableRefObject<HTMLDivElement>;
+	const { className, sparkline } = props;
+	const { t } = useTranslation();
+	const chartContainerRef = useRef() as MutableRefObject<HTMLDivElement>;
 
-    useEffect(() => {
-        const chartOptions = {
-            layout: {
-                textColor: 'black',
-                background: { type: ColorType.Solid, color: 'white' },
-            },
-            height: 260,
-            width: 500,
-            localization: {
+	useEffect(() => {
+		const chartOptions = {
+			layout: {
+				textColor: 'black',
+				background: { type: ColorType.Solid, color: 'white' },
+			},
+			height: 260,
+			width: 500,
+			localization: {
             	timeFormatter: (businessDayOrTimestamp) => {
-                    if (isUTCTimestamp(businessDayOrTimestamp)) {
-                        return new Date(businessDayOrTimestamp).toString();
-                    }
-                },
-            },
-        } as TimeChartOptions;
-        const chart = createChart(chartContainerRef.current, chartOptions);
-        const lineSeries = chart.addLineSeries({ color: '#2962FF' });
+					if (isUTCTimestamp(businessDayOrTimestamp)) {
+						return new Date(businessDayOrTimestamp).toString();
+					}
+				},
+			},
+		} as TimeChartOptions;
+		const chart = createChart(chartContainerRef.current, chartOptions);
+		const lineSeries = chart.addLineSeries({ color: '#2962FF' });
 
-        const time = sparkline.map((_, index) => (
+		const time = sparkline.map((_, index) => (
             Date.now() - (3600 * (index + 1)) * 1000 as UTCTimestamp
-        )).reverse();
+		)).reverse();
 
-        const coinData = sparkline.map((value, index) => ({ time: time[index], value: Number(value) }));
+		const coinData = sparkline.map((value, index) => ({ time: time[index], value: Number(value) }));
 
-        chart.timeScale().fitContent();
+		chart.timeScale().fitContent();
 
-        lineSeries.setData(coinData);
+		lineSeries.setData(coinData);
 
-        return () => {
-            chart.remove();
-        };
-    }, [sparkline]);
+		return () => {
+			chart.remove();
+		};
+	}, [sparkline]);
 
-    return (
-        <div className={classnames(classes.Chart, className)}>
-            <h1 className={classes.chartTitle}>{t('Last 24 hour chart')}</h1>
-            <div ref={chartContainerRef} />
-        </div>
-    );
+	return (
+		<div className={classnames(classes.Chart, className)}>
+			<h1 className={classes.chartTitle}>{t('Last 24 hour chart')}</h1>
+			<div ref={chartContainerRef} />
+		</div>
+	);
 };

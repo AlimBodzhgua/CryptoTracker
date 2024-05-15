6d8b0ece200ef50bd92be539f628ec27a8a87efd
AlimBodzhgua <bodzhgua2015@mail.ru>
2024-05-15 10:31:16 +0300

    lint fix

--- src/components/PageError/PageError.stories.tsx
+++ src/components/PageError/PageError.stories.tsx
@@ -3,18 +3,18 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { PageError } from './PageError';
 
 const meta = {
-    title: 'Components/PageError',
-    component: PageError,
-    parameters: {
-        layout: 'centered',
-    },
-    tags: ['autodocs'],
-    argTypes: {},
+	title: 'Components/PageError',
+	component: PageError,
+	parameters: {
+		layout: 'centered',
+	},
+	tags: ['autodocs'],
+	argTypes: {},
 } satisfies Meta<typeof PageError>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
-    args: {},
+	args: {},
 };
--- src/components/PageError/PageError.tsx
+++ src/components/PageError/PageError.tsx
@@ -10,25 +10,21 @@ interface PageErrorProps {
 }
 
 export const PageError: React.FC<PageErrorProps> = (props) => {
-    const { className } = props;
-    const { t } = useTranslation();
+	const { className } = props;
+	const { t } = useTranslation();
 
-    const onReload = () => location.reload();
+	const onReload = () => location.reload();
 
-    return (
-        <div className={classnames(classes.PageError, className)}>
-            <p
-                className={classes.title}
-            >
-                {t('An unexpected error occured')}
-            </p>
-            <Button
-                className={classes.reload}
-                onClick={onReload}
-                theme={ButtonTheme.secondary}
-            >
-                {t('Reload the page')}
-            </Button>
-        </div>
-    );
+	return (
+		<div className={classnames(classes.PageError, className)}>
+			<p className={classes.title}>{t('An unexpected error occured')}</p>
+			<Button
+				className={classes.reload}
+				onClick={onReload}
+				theme={ButtonTheme.secondary}
+			>
+				{t('Reload the page')}
+			</Button>
+		</div>
+	);
 };

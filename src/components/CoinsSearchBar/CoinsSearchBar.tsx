18ded5bfd1445eb04a376805a53504a272a8ef27
Alim <bodzhgua2015@mail.ru>
2025-01-17 21:54:03 +0300

    search logic moved to useMemo, deleted extra useEffect, added on escape key press input blur

--- src/components/CoinsSearchBar/CoinsSearchBar.tsx
+++ src/components/CoinsSearchBar/CoinsSearchBar.tsx
@@ -1,6 +1,4 @@
-import React, {
-	useState, useEffect, memo, useCallback, useRef,
-} from 'react';
+import { FC, useState, useEffect, memo, useCallback, useRef, useMemo } from 'react';
 import { useAppSelector, useAppDispatch } from 'hooks/redux';
 import { useDebounce } from 'hooks/useDebounce';
 import { selectCoins } from 'store/selectors/coinsSelectors';
@@ -15,22 +13,18 @@ interface CoinsSearchBarProps {
 	className?: string;
 }
 
-export const CoinsSearchBar: React.FC<CoinsSearchBarProps> = memo((props) => {
+export const CoinsSearchBar: FC<CoinsSearchBarProps> = memo((props) => {
 	const { className } = props;
 	const [searchQuery, setSearchQuery] = useState<string>('');
 	const dispatch = useAppDispatch();
 	const coins = useAppSelector(selectCoins);
 	const debouncedValue = useDebounce<string>(searchQuery);
-	const inputRef = useRef<HTMLInputElement>(null);
+	const inputRef = useRef<HTMLInputElement | null>(null);
 
-	useEffect(() => {
-		const searchedCoins = coins?.filter((coin) => {
-			if (coin.name.toLowerCase().includes(searchQuery.toLowerCase())) {
-				return coin;
-			}
-		});
-		dispatch(coinsActions.setSearchedFilteredCoins(searchedCoins));
-	}, [debouncedValue, coins]);
+	const searchResult = useMemo(
+		() => coins.filter((coin) =>coin.name.toLowerCase().includes(searchQuery.toLowerCase())),
+		[debouncedValue, coins],
+	);
 
 	const onHotkeysPress = useCallback((e: KeyboardEvent) => {
 		if (e.altKey === true && e.key === 'Enter') {
@@ -39,14 +33,17 @@ export const CoinsSearchBar: React.FC<CoinsSearchBarProps> = memo((props) => {
 			} else {
 				inputRef.current?.focus();
 			}
+		} else if (e.key === 'Escape' && document.activeElement === inputRef.current) {
+			inputRef.current?.blur();
 		}
 	}, []);
 
 	useEffect(() => {
+		dispatch(coinsActions.setSearchedFilteredCoins(searchResult));
 		window.addEventListener('keydown', onHotkeysPress);
 
 		return () => window.removeEventListener('keydown', onHotkeysPress);
-	}, []);
+	}, [searchResult, onHotkeysPress, dispatch]);
 
 	const onSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setSearchQuery(e.target.value);

aa628dc420a76255bbd6395eabc6fa54f0c4a6f1
Alim <bodzhgua2015@mail.ru>
2025-01-20 13:26:07 +0300

    changed table skeleton

--- src/components/CoinTable/CoinTable/CoinTableSkeleton.tsx
+++ src/components/CoinTable/CoinTable/CoinTableSkeleton.tsx
@@ -3,6 +3,7 @@ import { Skeleton } from 'components/UI/Skeleton/Skeleton';
 
 import classnames from 'classnames';
 import classes from './CoinTable.module.scss';
+import { useTranslation } from 'react-i18next';
 
 interface CoinTableSkeletonProps {
 	withHeader: boolean;
@@ -11,6 +12,7 @@ interface CoinTableSkeletonProps {
 
 export const CoinTableSkeleton: FC<CoinTableSkeletonProps> = memo((props) => {
 	const { withHeader, className } = props;
+	const { t } = useTranslation();
 
 	return (
 		<table
@@ -19,22 +21,60 @@ export const CoinTableSkeleton: FC<CoinTableSkeletonProps> = memo((props) => {
 			{withHeader && (
 				<thead className={classes.header}>
 					<tr className={classes.row}>
-						{new Array(7).fill(0).map((_, index) => (
-							// eslint-disable-next-line react/no-array-index-key
-							<th key={index}>
-								<Skeleton width={125} height={30} radius='10px' />
-							</th>
-						))}
+						<th className={classes.colHeader}>
+							<div>
+								<span>#</span>
+							</div>
+						</th>
+						<th className={classes.colHeader}>
+							<div className={classes.headerItem}>
+								<span>{t('Name')}</span>
+							</div>
+						</th>
+						<th className={classes.colHeader}>
+							<div className={classes.headerItem}>
+								<span>{t('Price')}</span>
+							</div>
+						</th>
+						<th className={classes.colHeader}>
+							<div className={classes.headerItem}>
+								<span>{t('Change')}</span>
+							</div>
+						</th>
+						<th className={classes.colHeader}>
+							<div className={classes.headerItem}>
+								<span>{t('24h volume')}</span>
+							</div>
+						</th>
+						<th className={classes.colHeader} colSpan={-1}>
+							<div className={classes.headerItem}>
+								<span>{t('Market cap')}</span>
+							</div>
+						</th>
+						<th className={classes.colHeader}>
+							<div className={classes.headerItem}>
+								<span>{t('Actions')}</span>
+							</div>
+						</th>
 					</tr>
 				</thead>
 			)}
 			<tbody>
 				{new Array(12).fill(0).map((_, index) => (
 					// eslint-disable-next-line react/no-array-index-key
-					<tr key={index}>
-						<th colSpan={7}>
-							<Skeleton width='100%' height={35} radius='10px' />
-						</th>
+					<tr key={index} className={classes.loadingRow}>
+						<td key={index}>
+							<Skeleton width={30} height={25} radius='5px' />
+						</td>
+						{new Array(5).fill(0).map((_, index) => (
+							// eslint-disable-next-line react/no-array-index-key
+							<td key={index}>
+								<Skeleton width={150} height={25} radius='5px' />
+							</td>
+						))}
+						<td key={index}>
+							<Skeleton width={35} height={25} radius='5px' />
+						</td>
 					</tr>
 				))}
 			</tbody>

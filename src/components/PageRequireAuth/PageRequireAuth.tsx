447daf906f9720776703693aaf5c802df6948a49
Alim <bodzhgua2015@mail.ru>
2025-01-21 00:06:58 +0300

    added lock icon, changed text styles

--- src/components/PageRequireAuth/PageRequireAuth.tsx
+++ src/components/PageRequireAuth/PageRequireAuth.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { Page } from 'components/UI/Page/Page';
 import { useTranslation } from 'react-i18next';
+import LockIcon from 'assets/icons/lock.svg'
 import classnames from 'classnames';
 import classes from './PageRequireAuth.module.scss';
 
@@ -13,9 +14,10 @@ export const PageRequireAuth: FC<PageRequireAuthProps> = ({ className }) => {
 
 	return (
 		<Page className={classnames(classes.PageRequierAuth, className)}>
-			<h1 className={classes.header}>
+			<LockIcon className={classes.lockIcon}/>
+			<h2 className={classes.header}>
 				{t('This page require auth.')}
-			</h1>
+			</h2>
 			<div className={classes.redirectText}>
 				{t('Soon you will redirect to the main page.')}
 			</div>

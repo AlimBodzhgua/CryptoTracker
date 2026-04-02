0716542546f18497f115d0b5184658d2f1fa5af3
AlimBodzhgua <bodzhgua2015@mail.ru>
2026-04-02 20:57:04 +0300

    fixed translation keys, changed ui

--- src/shared/UI/PageError/PageError.tsx
+++ src/shared/UI/PageError/PageError.tsx
@@ -1,29 +1,29 @@
-import React from 'react';
+import type { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Button } from 'shared/UI/Button/Button';
-
 import classnames from 'classnames';
+
 import classes from './PageError.module.scss';
 
 interface PageErrorProps {
 	className?: string;
 }
 
-export const PageError: React.FC<PageErrorProps> = (props) => {
-	const { className } = props;
+export const PageError: FC<PageErrorProps> = ({ className }) => {
 	const { t } = useTranslation();
 
 	const onReload = () => location.reload();
 
 	return (
 		<div className={classnames(classes.PageError, className)}>
-			<p className={classes.title}>{t('An unexpected error occurred')}</p>
+			<p className={classes.title}>{t('errors.unexpected_general')}</p>
 			<Button
 				className={classes.reload}
 				onClick={onReload}
 				theme='secondary'
+				size='big'
 			>
-				{t('Reload the page')}
+				{t('buttons.reload_page')}
 			</Button>
 		</div>
 	);
